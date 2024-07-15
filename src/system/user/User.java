48ff8c2be73c74125f87b0c880d35af0084f1c3e
Egor Vovk <vovk0001@algonquinlive.com>
2024-07-15 16:25:26 -0400

    Test3

--- src/system/user/User.java
+++ src/system/user/User.java
@@ -2,15 +2,31 @@ package system.user;
 
 import system.book.Book;
 import system.exception.UserException;
-
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
+
 
 public class User {
     private String email;
     private String password;
     private List<Book> bookList = new ArrayList<>();
     private UserPlan plan;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public User(String email, String password, UserPlan plan) {
         this.email = email;
